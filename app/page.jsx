85a0b35d95f9e72bb7ddca2b3e83db62cc3f8f24
vinitSanctumcloud <vinit1222003@gmail.com>
2025-06-25 17:49:31 +0530

    demo

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [open, setOpen] = useState(false);
 
   return (
-    <main className="w-[400px] h-[590px] flex items-center justify-center  rounded-xl shadow-md">
+    <main className="w-[400px] h-[690px] flex items-center justify-center  rounded-xl shadow-md mx-auto mt-[10%]">
       <ChatbotWidget />
     </main>
   );

9141ca2aed0a4a490af9c3983acb346c3967eb08
vinitSanctumcloud <vinit1222003@gmail.com>
2025-06-25 17:45:17 +0530

    dsdfs

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
-'use client';
-import { useState } from 'react';
-import ChatbotWidget from './ChatbotWidget';
+"use client";
+import { useState } from "react";
+import ChatbotWidget from "./ChatbotWidget";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
 
   return (
-    <main className="min-h-screen flex items-center justify-center ">
-      
-
+    <main className="w-[400px] h-[590px] flex items-center justify-center  rounded-xl shadow-md">
       <ChatbotWidget />
     </main>
   );
